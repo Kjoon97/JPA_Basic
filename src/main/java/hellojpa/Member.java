944126cfac390f08c6ccdfb15b7bdadc439456ad
joonhyuck <jh83370@naver.com>
2022-03-26 18:23:11 +0900

    다대다[N:M]

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,7 +1,9 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -25,6 +27,9 @@ public class Member {
     @OneToOne
     @JoinColumn(name="LOCKER_ID")
     private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> products = new ArrayList<>();
 
     public Long getId() {
         return id;
